64e10db15876e9db023f6599da15600a89c77c16
gyuseon <mn5592@naver.com>
2024-11-01 15:14:57 +0900

    Chore: 스프링 부트 프로젝트 생성 (#1)

---
+++ src/main/java/com/adoonge/seedzip/SeedzipApplication.java
@@ -0,0 +1,13 @@
+package com.adoonge.seedzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedzipApplication.class, args);
+	}
+
+}
