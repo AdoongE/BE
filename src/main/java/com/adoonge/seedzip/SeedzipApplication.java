1bcf0df48fecdecd2ae1db4bc8eb4be655c1db42
gyuseon <mn5592@naver.com>
2024-11-02 19:32:46 +0900

    Fix: @EnableJpaAuditing 애노테이션 추가 (#3)

--- src/main/java/com/adoonge/seedzip/SeedzipApplication.java
+++ src/main/java/com/adoonge/seedzip/SeedzipApplication.java
@@ -2,8 +2,10 @@ package com.adoonge.seedzip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SeedzipApplication {
 
 	public static void main(String[] args) {
