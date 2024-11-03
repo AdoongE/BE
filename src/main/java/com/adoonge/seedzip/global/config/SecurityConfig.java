7e60a8c0b6b117c22572b4f2353ea64ed3c30db2
gyuseon <mn5592@naver.com>
2024-11-03 20:37:10 +0900

    Feat: JWT 토큰 관련 기능 구현 (#3)

---
+++ src/main/java/com/adoonge/seedzip/global/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.adoonge.seedzip.global.config;
+
+import com.adoonge.seedzip.auth.filter.JwtAuthenticationFilter;
+import com.adoonge.seedzip.auth.service.JwtTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenService jwtTokenService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                // REST API이므로 basic auth 및 csrf 보안을 사용하지 않음
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                // JWT를 사용하기 때문에 세션을 사용하지 않음
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v1/api-docs/**","/health")
+                                .permitAll()
+                                // 해당 API에 대해서는 모든 요청을 허가
+                                .requestMatchers("/api/v1/auth/login","/api/v1/auth/signup").permitAll()
+                                //.requestMatchers("/members/test").hasRole("USER")
+                                // USER 권한이 있어야 요청할 수 있음
+                                //.requestMatchers("/members/test").hasRole("USER")
+                                // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
+                                .anyRequest().authenticated())
+                // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // BCrypt Encoder 사용
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+
+}
