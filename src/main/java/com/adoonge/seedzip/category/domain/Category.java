ce7895e0e3daaf9a8e0947456d4f9c3d8d75dbb9
chaen-ing <kimce0405@naver.com>
2024-11-06 01:41:34 +0900

    Feat : 카테고리 엔티티 생성

---
+++ src/main/java/com/adoonge/seedzip/category/domain/Category.java
@@ -0,0 +1,41 @@
+package com.adoonge.seedzip.category.domain;
+
+import com.adoonge.seedzip.member.domain.Member;
+import com.fasterxml.jackson.databind.ser.Serializers;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Category extends Serializers.Base {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long categoryId;
+
+	private String name;
+
+	@Enumerated(EnumType.STRING)
+	private Visibility isPublic;
+
+	@ManyToOne
+	@JoinColumn(name = "memberId")
+	private Member member;
+}
