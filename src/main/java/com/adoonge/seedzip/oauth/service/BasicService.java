245c05eab8e81d3ee0e2d05c64889f65fbfa6033
gyuseon <mn5592@naver.com>
2024-11-06 00:51:05 +0900

    Refactor: 회원가입 리팩토링 (#6)

--- src/main/java/com/adoonge/seedzip/oauth/service/BasicService.java
+++ src/main/java/com/adoonge/seedzip/oauth/service/BasicService.java
@@ -1,6 +1,5 @@
 package com.adoonge.seedzip.oauth.service;
 
-import com.adoonge.seedzip.auth.dto.request.LoginRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +8,17 @@ import org.springframework.stereotype.Service;
 public class BasicService implements OAuthService{
 
     @Override
-    public String getLoginId(String accessCode) {
-        return accessCode;
+    public String getAccessToken(String code) {
+        return code;
     }
 
     @Override
-    public String getProfileImageUrl(String socialAccessCode) {
+    public String getLoginId(String loginId) {
+        return loginId;
+    }
+
+    @Override
+    public String getProfileImageUrl(String loginId) {
         //추후에 기본 프로필 이미지 링크 추가
         return "";
     }
