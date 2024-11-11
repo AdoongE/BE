f944f0b26eb33fceafd07fe99cebf996cf6820c6
gyuseon <mn5592@naver.com>
2024-11-12 03:29:50 +0900

    Feat: CORS 설정 (#28)

--- src/main/java/com/adoonge/seedzip/global/config/SecurityConfig.java
+++ src/main/java/com/adoonge/seedzip/global/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.adoonge.seedzip.global.config;
 
 import com.adoonge.seedzip.auth.filter.JwtAuthenticationFilter;
 import com.adoonge.seedzip.auth.service.JwtTokenService;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,9 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -41,6 +45,8 @@ public class SecurityConfig {
                                 //.requestMatchers("/members/test").hasRole("USER")
                                 // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
                                 .anyRequest().authenticated())
+                // CORS 설정을 수동으로 추가
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class)
                 .build();
@@ -52,5 +58,17 @@ public class SecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-
+    // CORS 설정을 위한 Bean 정의
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 프론트엔드 도메인
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")); // 허용할 HTTP 메서드
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // 허용할 헤더
+        configuration.setExposedHeaders(Arrays.asList("Authorization")); // 응답에서 노출할 헤더
+        configuration.setAllowCredentials(true); // 자격 증명 포함 요청 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+        return source;
+    }
 }
