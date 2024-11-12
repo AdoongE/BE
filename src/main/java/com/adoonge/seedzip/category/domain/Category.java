461a54173e617738a342369f86981bcc36976c42
chaen-ing <kimce0405@naver.com>
2024-11-12 14:45:09 +0900

    Feat : 카테고리 디폴트 이름 (#5)

--- src/main/java/com/adoonge/seedzip/category/domain/Category.java
+++ src/main/java/com/adoonge/seedzip/category/domain/Category.java
@@ -1,9 +1,12 @@
 package com.adoonge.seedzip.category.domain;
 
+import org.hibernate.annotations.ColumnDefault;
+
 import com.adoonge.seedzip.global.entity.BaseEntity;
 import com.adoonge.seedzip.member.domain.Member;
 import com.fasterxml.jackson.databind.ser.Serializers;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -13,6 +16,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -29,14 +33,23 @@ public class Category extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long categoryId;
 
+	@Column(length = 50)
 	private String name;
 
 	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private Visibility visibility;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
+
+	@PrePersist
+	public void setDefaultValues() {
+		if (this.name == null) {
+			this.name = "새 카테고리";
+		}
+	}
 
 	public void updateCategory(String name) {
 		this.name = name;
