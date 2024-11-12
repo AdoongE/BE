19098db60288adcb74dc5825259e1cee446687ab
chaen-ing <kimce0405@naver.com>
2024-11-13 02:28:34 +0900

    Feat : 콘텐츠 상세 조회 image, pdf (#8)

--- src/main/java/com/adoonge/seedzip/content/repository/DocumentRepository.java
+++ src/main/java/com/adoonge/seedzip/content/repository/DocumentRepository.java
@@ -1,9 +1,15 @@
 package com.adoonge.seedzip.content.repository;
 
+import java.util.List;
+
 import com.adoonge.seedzip.content.domain.Document;
+import com.adoonge.seedzip.content.domain.Image;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+
+	List<Document> findAllByContents_ContentsId(Long contentsId);
 }
--- src/main/java/com/adoonge/seedzip/content/repository/ImageRepository.java
+++ src/main/java/com/adoonge/seedzip/content/repository/ImageRepository.java
@@ -1,14 +1,21 @@
 package com.adoonge.seedzip.content.repository;
 
 import com.adoonge.seedzip.content.domain.Image;
+import com.adoonge.seedzip.content.domain.Link;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
     @Query("SELECT i FROM Image i WHERE i.contents.contentsId = :contentsId AND i.imgThumbnail = :imgThumbnail")
     Optional<Image> findByContentsIdAndImgThumbnail(Long contentsId, boolean imgThumbnail);
+
+    List<Image> findAllByContents_ContentsId(Long contentsId);
+
+
 }
