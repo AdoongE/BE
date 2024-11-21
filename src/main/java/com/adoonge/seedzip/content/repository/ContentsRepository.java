5c2b3e79aec6c55b57bed966a0201578cef105ff
plum-king <jeesoo0908@gmail.com>
2024-11-22 00:58:33 +0900

    Feat : 콘텐츠 최신순으로 조회 가능하도록 수정 (#8)

--- src/main/java/com/adoonge/seedzip/content/repository/ContentsRepository.java
+++ src/main/java/com/adoonge/seedzip/content/repository/ContentsRepository.java
@@ -2,6 +2,7 @@ package com.adoonge.seedzip.content.repository;
 
 import com.adoonge.seedzip.content.domain.Contents;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,10 @@ import java.util.Optional;
 @Repository
 public interface ContentsRepository extends JpaRepository<Contents, Long> {
     Contents findByContentsId(Long contentsId);
+
+    @Query("SELECT c FROM Contents c WHERE c.member.id = :memberId ORDER BY c.contentsId DESC")
     List<Contents> findByMemberId(Long memberId);
+
+    @Query("SELECT c FROM Contents c WHERE c.contentsId IN :contentIds ORDER BY c.contentsId DESC")
     List<Contents> findByContentsIdIn(List<Long> contentIds);
 }
