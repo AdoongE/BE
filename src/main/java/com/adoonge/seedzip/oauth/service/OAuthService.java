21d9e21c708607c178f7437b7b765d6866c850de
gyuseon <mn5592@naver.com>
2024-11-28 01:14:33 +0900

    Feat: OAuthService에 Naver 추가 (#28)

--- src/main/java/com/adoonge/seedzip/oauth/service/OAuthService.java
+++ src/main/java/com/adoonge/seedzip/oauth/service/OAuthService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface OAuthService {
-    String getAccessToken(String socialCode);
+    String getAccessToken(String socialCode); //state는 네이버 로그인시만 필요
     String getLoginId(String socialAccessToken);
     String getProfileImageUrl(String socialAccessToken);
 }
