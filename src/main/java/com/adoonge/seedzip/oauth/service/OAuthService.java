dd79f9b126c5144c5a7fdc98c3ee95faabcc6c65
gyuseon <mn5592@naver.com>
2025-03-13 18:21:51 +0900

    Refactor: 소셜 로그인 인터페이스 리팩토링 (#118)

--- src/main/java/com/adoonge/seedzip/oauth/service/OAuthService.java
+++ src/main/java/com/adoonge/seedzip/oauth/service/OAuthService.java
@@ -1,9 +1,13 @@
 package com.adoonge.seedzip.oauth.service;
 
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface OAuthService {
+    ResponseEntity<Map> requestSocialUserAccessToken(String code);
+    ResponseEntity<Map> requestSocialUserAccessInfo(String socialAccessToken);
     String getAccessToken(String socialCode); //state는 네이버 로그인시만 필요
     String getLoginId(String socialAccessToken);
     String getProfileImageUrl(String socialAccessToken);
