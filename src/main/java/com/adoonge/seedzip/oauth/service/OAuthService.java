af1aa48dc69fb22292639b08b315cd4ab53f2640
gyuseon <mn5592@naver.com>
2025-03-13 18:36:50 +0900

    Refactor: KakaoService 리팩토링 (#118)

--- src/main/java/com/adoonge/seedzip/oauth/service/OAuthService.java
+++ src/main/java/com/adoonge/seedzip/oauth/service/OAuthService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public interface OAuthService {
     ResponseEntity<Map> requestSocialUserAccessToken(String code);
-    ResponseEntity<Map> requestSocialUserAccessInfo(String socialAccessToken);
+    ResponseEntity<Map> requestSocialUserInfo(String socialAccessToken);
     String getSocialAccessToken(String socialCode); //state는 네이버 로그인시만 필요
     String getLoginId(String socialAccessToken);
     String getProfileImageUrl(String socialAccessToken);
