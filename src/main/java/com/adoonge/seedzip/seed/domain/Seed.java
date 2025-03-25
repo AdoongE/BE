7c757757d32e4983525d9165fae675577010897a
gyuseon <mn5592@naver.com>
2025-03-25 23:50:53 +0900

    Feat: Seed entity 생성 (#121)

---
+++ src/main/java/com/adoonge/seedzip/seed/domain/Seed.java
@@ -0,0 +1,62 @@
+package com.adoonge.seedzip.seed.domain;
+
+import com.adoonge.seedzip.global.entity.BaseEntity;
+import com.adoonge.seedzip.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "seeds")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Seed extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seedId;
+
+    private String seedName;
+
+    private LocalDate dDay;
+
+    private String seedDetail;
+
+    @Enumerated(EnumType.STRING)
+    private SeedType seedType;
+
+    private int thumbnailIdx;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @OnDelete(action = OnDeleteAction.CASCADE) // Member 삭제 시 Content도 삭제
+    private Member member;
+
+    @Column(nullable = false, length = 1000)
+    private String link; // (링크 -> 입력으로 들어온 링크), (이미지, PDF -> s3 저장 링크)
+
+    @Column(nullable = false)
+    private String fileName; // 파일명 (이미지, PDF, 링크는 null 값)
+
+    @Column(nullable = false)
+    private Boolean fileThumbnail; // 썸네일 여부 (이미지, PDF, 링크는 null 값)
+}
