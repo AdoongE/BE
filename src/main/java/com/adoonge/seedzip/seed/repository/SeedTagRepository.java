96d5be59720bc71926843f49aada6b51d00ccd2a
gyuseon <mn5592@naver.com>
2025-03-26 00:00:05 +0900

    Feat: 매핑 관련 엔티티 SeedTag, Repository 생성 (#121)

---
+++ src/main/java/com/adoonge/seedzip/seed/repository/SeedTagRepository.java
@@ -0,0 +1,7 @@
+package com.adoonge.seedzip.seed.repository;
+
+import com.adoonge.seedzip.seed.domain.mapping.SeedTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeedTagRepository extends JpaRepository<SeedTag, Long> {
+}
