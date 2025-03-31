052e5183e707e79ca8b59d826223fad49bd0a283
chaen-ing <kimce0405@naver.com>
2025-03-31 18:46:18 +0900

    Refact : 엔티티 구조 변경 (#121)

---
+++ src/main/java/com/adoonge/seedzip/seed/domain/File.java
@@ -0,0 +1,51 @@
+package com.adoonge.seedzip.seed.domain;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.adoonge.seedzip.content.domain.Contents;
+import com.adoonge.seedzip.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "files")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class File extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "file_id")
+	private Long id;
+
+	@Column(nullable = false, length = 1000)
+	private String link;	// 이미지, pdf는 aws s3 링크
+
+	private String fileName;
+
+	@Column(nullable = false)
+	private SeedType seedType;
+
+	private Boolean isThumbnail;	// 링크, pdf는 null
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "seed_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Contents contents;
+}
