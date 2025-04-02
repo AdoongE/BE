2cf920b332c31f1faf25d873bb37b3503326d5f0
gyuseon <mn5592@naver.com>
2025-04-02 20:59:50 +0900

    Feat: - Seed로 Category Id 리스트가져오는 메소드 작성 - Seed로 Category Name 리스트 가져오는 메소드 작성 (#121)

--- src/main/java/com/adoonge/seedzip/seed/repository/CategorySeedRepository.java
+++ src/main/java/com/adoonge/seedzip/seed/repository/CategorySeedRepository.java
@@ -1,7 +1,18 @@
 package com.adoonge.seedzip.seed.repository;
 
+import com.adoonge.seedzip.seed.domain.Seed;
 import com.adoonge.seedzip.seed.domain.mapping.CategorySeed;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CategorySeedRepository extends JpaRepository<CategorySeed, Long> {
+
+    @Query("SELECT cs.category.categoryId FROM CategorySeed cs WHERE cs.seed = :seed")
+    List<Long> findCategoryIdsBySeed(@Param("seed") Seed seed);
+
+    // 자동으로 Inner 조인 사용 , n+1 문제 발생 X
+    @Query("SELECT cs.category.name FROM CategorySeed cs WHERE cs.seed = :seed")
+    List<String> findCategoryNamesBySeed(@Param("seed") Seed seed);
 }
