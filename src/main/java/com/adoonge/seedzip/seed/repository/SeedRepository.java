d2a187e387c655c9cc4d8d38730a78be239f9dcb
gyuseon <mn5592@naver.com>
2025-04-02 21:01:33 +0900

    Feat: 멤버의 씨드를 페이징해서 가져오는 메소드 구현(#121)

--- src/main/java/com/adoonge/seedzip/seed/repository/SeedRepository.java
+++ src/main/java/com/adoonge/seedzip/seed/repository/SeedRepository.java
@@ -1,7 +1,13 @@
 package com.adoonge.seedzip.seed.repository;
 
+import com.adoonge.seedzip.member.domain.Member;
 import com.adoonge.seedzip.seed.domain.Seed;
+import com.adoonge.seedzip.seed.domain.SeedType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeedRepository extends JpaRepository<Seed, Long> {
+    Page<Seed> findByMember(Member member, Pageable pageable);
+    Page<Seed> findByMemberAndSeedType(Member member, SeedType seedType, Pageable pageable);
 }
