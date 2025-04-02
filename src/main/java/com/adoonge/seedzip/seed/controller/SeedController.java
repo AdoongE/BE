cf911d278d03977822476136618868740d1c10f0
gyuseon <mn5592@naver.com>
2025-04-02 21:01:49 +0900

    Feat: 전체 씨드 모아보기 기능 구현(#121)

--- src/main/java/com/adoonge/seedzip/seed/controller/SeedController.java
+++ src/main/java/com/adoonge/seedzip/seed/controller/SeedController.java
@@ -1,9 +1,20 @@
 package com.adoonge.seedzip.seed.controller;
 
+import com.adoonge.seedzip.auth.util.CustomUserDetails;
+import com.adoonge.seedzip.global.dto.response.ApiResponse;
+import com.adoonge.seedzip.member.domain.Member;
+import com.adoonge.seedzip.seed.dto.response.SeedResponse;
 import com.adoonge.seedzip.seed.service.SeedService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +24,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class SeedController {
 
     private final SeedService seedService;
+
+    @GetMapping
+    @Operation(summary = "전체 씨드 모아보기 API", description = "전체 씨드를 홈화면에서 조회하는 API입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공적으로 업로드됨",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = SeedResponse.getAllSeeds.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    public ApiResponse<SeedResponse.getAllSeeds> getAllSeeds(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "latest") String sortBy, // latest or name
+            @RequestParam(defaultValue = "false") boolean isAsc,
+            @RequestParam(required = false) String seedType,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+
+        // 빈 값 처리
+        if (sortBy.isBlank()) sortBy = "latest";
+
+        Member member = customUserDetails.getMember();
+
+        SeedResponse.getAllSeeds getAllSeeds = seedService.getAllSeeds(member, page, size, sortBy, isAsc, seedType);
+        return new ApiResponse<>(getAllSeeds);
+    }
+
 }
--- src/main/java/com/adoonge/seedzip/seed/service/SeedService.java
+++ src/main/java/com/adoonge/seedzip/seed/service/SeedService.java
@@ -1,9 +1,31 @@
 package com.adoonge.seedzip.seed.service;
 
+import com.adoonge.seedzip.content.dto.response.ContentsAllResponse;
+import com.adoonge.seedzip.global.exception.ErrorCode;
+import com.adoonge.seedzip.global.exception.SeedzipException;
+import com.adoonge.seedzip.member.domain.Member;
+import com.adoonge.seedzip.seed.domain.File;
+import com.adoonge.seedzip.seed.domain.Seed;
+import com.adoonge.seedzip.seed.domain.SeedType;
+import com.adoonge.seedzip.seed.dto.response.SeedResponse;
+import com.adoonge.seedzip.seed.dto.response.SeedResponse.getAllSeeds;
+import com.adoonge.seedzip.seed.repository.CategorySeedRepository;
+import com.adoonge.seedzip.seed.repository.FileRepository;
 import com.adoonge.seedzip.seed.repository.SeedRepository;
+import com.adoonge.seedzip.seed.repository.SeedTagRepository;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,4 +33,121 @@ import org.springframework.stereotype.Service;
 public class SeedService {
 
     private final SeedRepository seedRepository;
+    private final FileRepository fileRepository;
+    private final CategorySeedRepository categorySeedRepository;
+    private final SeedTagRepository seedTagRepository;
+
+    @Transactional(readOnly = true)
+    public SeedResponse.getAllSeeds getAllSeeds(Member member, int page, int size, String sortBy, boolean isAsc, String seedType) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+
+        String sortField = switch (sortBy) {
+            case "name" -> "seedName";
+            case "latest" -> "createdAt";
+            default -> "createdAt";
+        };
+
+        // 문자열 -> Enum 변환 (대소문자 구분 없이 처리)
+        SeedType parsedSeedType = null;
+        if (seedType != null && !seedType.isBlank()) {
+            try {
+                parsedSeedType = SeedType.valueOf(seedType.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                throw SeedzipException.from(ErrorCode.INVALID_INPUT_VALUE);
+            }
+        }
+
+        //페이징을 위한 Pageable 객체
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField));
+
+        //페이징으로 얻어온 Seed 리스트
+        Page<Seed> seedList;
+        if (seedType != null) {
+            seedList = seedRepository.findByMemberAndSeedType(member, parsedSeedType, pageable);
+        } else {
+            seedList = seedRepository.findByMember(member, pageable);
+        }
+
+        if (seedList.isEmpty())
+            return null;
+
+        List<SeedResponse.seedInfo> seedInfoList = generateResponseFromSeedList(seedList.getContent());
+
+        //페이징 정보 추가
+        SeedResponse.PageInfo pageInfo = SeedResponse.PageInfo.builder()
+                .page(seedList.getNumber())
+                .size(seedList.getSize())
+                .totalPages(seedList.getTotalPages())
+                .totalElements(seedList.getTotalElements())
+                .isLast(seedList.isLast())
+                .build();
+
+        SeedResponse.getAllSeeds getAllSeeds = new getAllSeeds().builder()
+                .nickname(member.getNickname())
+                .seedInfoList(seedInfoList)
+                .pageInfo(pageInfo)
+                .build();
+
+        return getAllSeeds;
+    }
+
+    //List<Seed> -> List<SeedResponse.seedInfo>로 변환
+    private List<SeedResponse.seedInfo> generateResponseFromSeedList(List<Seed> seedList) {
+        //여기 작성하기 + 카테고리 이름이랑 태그 이름 어떻게 효과적으로 가져올지 찾아보기
+        return seedList.stream()
+                .map(seed -> {
+                    String thumbnailUrl = null;
+                    //링크의 경우 thumbnail 없으니까, 해당 링크를 thumbnailUrl로 처리
+                    if(seed.getSeedType().equals(SeedType.LINK)){
+                        Optional<File> thumbnailFile = fileRepository.findBySeed(seed);
+                        if(thumbnailFile.isPresent()){
+                            thumbnailUrl = thumbnailFile.get().getLink();
+                        }
+                    }
+                    // 이미지, PDF의 경우 isThumbnail이 true인 파일만 가져와서 s3 링크 추출
+                    else {
+                        Optional<File> thumbnailFile = fileRepository.findThumbnailBySeed(seed);
+                        if(thumbnailFile.isPresent()){
+                            thumbnailUrl = thumbnailFile.get().getLink();
+                        }
+                    }
+
+                    //seedId에 해당하는 카테고리 리스트 조회
+                    List<Long> categoryIds = categorySeedRepository.findCategoryIdsBySeed(seed);
+                    List<String> categoryNames = categorySeedRepository.findCategoryNamesBySeed(seed);
+
+                    //seedId에 해당하는 태그 리스트 조회
+                    List<Long> tagIds = seedTagRepository.findTagIdsBySeed(seed);
+                    List<String> tagNames = seedTagRepository.findTagNamesBySeed(seed);
+
+                    // D-day 계산
+                    int dDayValue = 1;
+                    if (seed.getDDay() != null) {
+                        LocalDate today = LocalDate.now();
+                        long daysBetween = ChronoUnit.DAYS.between(today, seed.getDDay());
+
+                        if (daysBetween > 0) {
+                            dDayValue = -(int)daysBetween;
+                        } else if (daysBetween == 0) {
+                            dDayValue = 0;
+                        }
+                    }
+
+                    // SeedResponse 객체에 필요한 정보 담기
+                    return new SeedResponse.seedInfo(
+                            seed.getId(),
+                            seed.getSeedName(),
+                            categoryIds,
+                            categoryNames,
+                            seed.getSeedType(),
+                            thumbnailUrl,
+                            seed.getUpdatedAt(),
+                            tagIds,
+                            tagNames,
+                            dDayValue
+                    );
+
+                })
+                .collect(Collectors.toList());
+    }
 }
