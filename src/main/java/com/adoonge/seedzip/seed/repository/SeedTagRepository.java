cbb1f2c81c3944d1807bb102706fe903e84dae78
gyuseon <mn5592@naver.com>
2025-04-02 21:00:04 +0900

    Feat: - Seed로 Tag Id 리스트가져오는 메소드 작성 - Seed로 Tag Name 리스트 가져오는 메소드 작성 (#121)

--- src/main/java/com/adoonge/seedzip/seed/repository/SeedTagRepository.java
+++ src/main/java/com/adoonge/seedzip/seed/repository/SeedTagRepository.java
@@ -1,7 +1,18 @@
 package com.adoonge.seedzip.seed.repository;
 
+import com.adoonge.seedzip.seed.domain.Seed;
 import com.adoonge.seedzip.seed.domain.mapping.SeedTag;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SeedTagRepository extends JpaRepository<SeedTag, Long> {
+
+    @Query("SELECT st.tag.id FROM SeedTag st WHERE st.seed = :seed")
+    List<Long> findTagIdsBySeed(@Param("seed") Seed seed);
+
+    // 자동으로 Inner 조인 사용 , n+1 문제 발생 X
+    @Query("SELECT st.tag.tagName FROM SeedTag st WHERE st.seed = :seed")
+    List<String> findTagNamesBySeed(@Param("seed") Seed seed);
 }
