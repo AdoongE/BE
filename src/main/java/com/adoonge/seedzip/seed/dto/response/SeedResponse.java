f1758343a43fa20d85c6df6271329d0dedf07f83
gyuseon <mn5592@naver.com>
2025-04-02 20:16:55 +0900

    Feat: SeedResponse 생성 (#121)

---
+++ src/main/java/com/adoonge/seedzip/seed/dto/response/SeedResponse.java
@@ -0,0 +1,51 @@
+package com.adoonge.seedzip.seed.dto.response;
+
+import com.adoonge.seedzip.content.domain.ContentsDataType;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class SeedResponse {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PageInfo {
+        int page;
+        int size;
+        int totalPages;
+        long totalElements;
+        boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class seedInfo{
+        Long seedId;
+        String seedName;
+        List<Long> categoryId;
+        List<String> categoryName;
+        ContentsDataType seedDateType;
+        String thumbnailImage; // 없으면 null
+        LocalDateTime updatedDt;
+        List<Long> tagId;
+        List<String> tagName;
+        int dDay;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class getAllSeeds{
+        String nickname;
+        List<seedInfo> seedInfoList;
+        PageInfo pageInfo;
+    }
+}
