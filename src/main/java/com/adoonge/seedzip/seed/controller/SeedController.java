a2a2354da700b1d766207f46a02ab96bbeb9c8a2
chaen-ing <kimce0405@naver.com>
2025-04-06 19:41:48 +0900

    Feat : 콘텐츠 생성 컨트롤러 작성 및 request 작성 (#121)

--- src/main/java/com/adoonge/seedzip/seed/controller/SeedController.java
+++ src/main/java/com/adoonge/seedzip/seed/controller/SeedController.java
@@ -2,7 +2,9 @@ package com.adoonge.seedzip.seed.controller;
 
 import com.adoonge.seedzip.auth.util.CustomUserDetails;
 import com.adoonge.seedzip.global.dto.response.ApiResponse;
+import com.adoonge.seedzip.global.exception.ErrorCode;
 import com.adoonge.seedzip.member.domain.Member;
+import com.adoonge.seedzip.seed.dto.reqeust.SeedRequest;
 import com.adoonge.seedzip.seed.dto.response.SeedResponse;
 import com.adoonge.seedzip.seed.service.SeedService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,10 +12,13 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,6 +74,23 @@ public class SeedController {
 
         SeedResponse.GetAllSeeds getAllSeeds = seedService.getCategorySeeds(member, page, size, sortBy, isAsc, seedType, categoryId);
         return new ApiResponse<>(getAllSeeds);
+    }
+
+    @PostMapping
+    @Operation(summary = "씨드 업로드 API", description = "씨드를 업로드하는 API입니다.")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공적으로 업로드됨",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = SeedResponse.GetAllSeeds.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    public ApiResponse<SeedResponse.SeedInfo> uploadSeed(
+            @RequestBody @Valid SeedRequest seedRequest,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+
+        Member member = customUserDetails.getMember();
+
+        return new ApiResponse<>(ErrorCode.REQUEST_OK);
     }
 
 }
