7c254ca6ce8a9918d527c724931b2563a1bf8d71
chaen-ing <kimce0405@naver.com>
2025-05-10 00:46:49 +0900

    Feat : 수정 로직 구현 2 (#131)

--- src/main/java/com/adoonge/seedzip/seed/service/SeedService.java
+++ src/main/java/com/adoonge/seedzip/seed/service/SeedService.java
@@ -18,6 +18,7 @@ import com.adoonge.seedzip.seed.dto.projection.SeedProjectionResult;
 import com.adoonge.seedzip.seed.dto.projection.SeedTagProjection;
 import com.adoonge.seedzip.seed.dto.request.SeedFilteringRequest;
 import com.adoonge.seedzip.seed.dto.request.SeedRequest;
+import com.adoonge.seedzip.seed.dto.request.SeedUpdateRequest;
 import com.adoonge.seedzip.seed.dto.response.SeedResponse;
 import com.adoonge.seedzip.seed.repository.CategorySeedRepository;
 import com.adoonge.seedzip.seed.repository.FileRepository;
@@ -36,6 +37,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -170,7 +173,7 @@ public class SeedService {
 
 	@Transactional
 	public SeedResponse.SeedInfoSimple uploadSeed(SeedRequest seedRequest, Member member) {
-		if(seedRequest.seedType() == SeedType.LINK && seedRequest.contentLink() == null) {
+		if(seedRequest.seedType() == SeedType.LINK && seedRequest.seedLink() == null) {
 			throw SeedzipException.from(ErrorCode.EMPTY_LINK);
 		}
 
@@ -181,7 +184,7 @@ public class SeedService {
 		saveSeedCategories(seedRequest.boardCategories(), member, seed);
 
 		if (seedRequest.seedType() == SeedType.LINK) {
-			fileService.saveLink(seedRequest.contentLink(), seed);
+			fileService.saveLink(seedRequest.seedLink(), seed);
 		}
 
 		return SeedResponse.SeedInfoSimple
@@ -202,8 +205,7 @@ public class SeedService {
 
 	@Transactional(readOnly = true)
 	public SeedResponse.SeedDetail getSeedDetail(Long seedId, Member member) {
-		Seed seed = seedRepository.findById(seedId)
-				.orElseThrow(() -> SeedzipException.from(ErrorCode.SEED_NOT_FOUND));
+		Seed seed = getSeedOrThrow(seedId);
 		List<File> files = fileRepository.findAllBySeed(seed)
 				.orElseThrow(() -> SeedzipException.from(ErrorCode.SEED_NOT_FOUND));
 
@@ -394,11 +396,11 @@ public class SeedService {
 
 		//콘텐츠 소유자 검증
 		if (!seed.getMember().getId().equals(member.getId())) {
-			throw SeedzipException.from(ErrorCode.CATEGORY_ACCESS_DENIED);
+			throw SeedzipException.from(ErrorCode.MEMBER_NOT_OWNER);
 		}
 
 		//카테고리 매핑 삭제
-		categorySeedRepository.deleteCategorySeedsBySeedId(id);
+		categorySeedRepository.deleteAllBySeedId(id);
 
 		//S3에서 파일 삭제
 		List<String> fileLinks = fileRepository.findFilesBySeedId(id).stream()
@@ -407,12 +409,75 @@ public class SeedService {
 		fileService.deleteFiles(seed.getSeedType(), fileLinks);
 
 		//파일 삭제
-		fileRepository.deleteFilesBySeedId(id);
+		fileRepository.deleteAllBySeedId(id);
 
 		//태그 매핑 삭제
-		seedTagRepository.deleteSeedTagsBySeedId(id);
+		seedTagRepository.deleteAllBySeedId(id);
 
 		seedRepository.delete(seed);
+	}
+
+	// 콘텐츠 수정
+	@Transactional
+	public SeedResponse.SeedInfoSimple updateSeed(SeedUpdateRequest request,
+		Long seedId, Member member) {
+		if(request.seedType() == SeedType.LINK && request.seedLink() == null) {
+			throw SeedzipException.from(ErrorCode.EMPTY_LINK);
+		}
+
+		Seed seed = getSeedOrThrow(seedId);
+		if(!request.seedType().equals(seed.getSeedType())) {
+			throw SeedzipException.from(ErrorCode.SEED_TYPE_NOT_SUPPORTED);
+		}
+
+		//콘텐츠 소유자 검증
+		if (!seed.getMember().getId().equals(member.getId())) {
+			throw SeedzipException.from(ErrorCode.MEMBER_NOT_OWNER);
+		}
+
+		// 기본 필드 수정
+		if (!Objects.equals(seed.getSeedName(), request.seedName())) {
+			seed.updateSeedName(request.seedName());
+		}
+		if (!Objects.equals(seed.getDDay(), request.dDay())) {
+			seed.updateDDay(request.dDay());
+		}
+		if (!Objects.equals(seed.getSeedDetail(), request.seedDetail())) {
+			seed.updateSeedDetail(request.seedDetail());
+		}
+		if (!Objects.equals(seed.getThumbnailIdx(), request.thumbnailImage())) {
+			seed.updateThumbnailIdx(request.thumbnailImage());
+		}
+
+		// 태그
+		seedTagRepository.deleteAllBySeedId(seedId);
+		saveSeedTags(request.tags(), member, seed);
+
+		// 카테고리
+		categorySeedRepository.deleteAllBySeedId(seedId);
+		saveSeedCategories(request.boardCategories(), member, seed);
+
+		if (seed.getSeedType().equals(SeedType.LINK)) {
+			File file = fileRepository.findBySeed(seed).orElseThrow(
+				() -> SeedzipException.from(ErrorCode.FILE_NOT_FOUND)
+			);
+			if (!Objects.equals(file.getLink(), request.seedLink())) {
+				file.updateLink(request.seedLink());
+			}
+		} else {    // 이미지, PDF
+			List<String> fileLinks = fileRepository.findFilesBySeedId(seedId).stream()
+				.map(File::getLink)
+				.toList();
+			fileService.deleteFiles(seed.getSeedType(), fileLinks);
+
+			fileRepository.deleteAllBySeedId(seedId);
+		}
+
+		Seed updatedSeed = seedRepository.save(seed);
+		return SeedResponse.SeedInfoSimple.builder()
+			.seedId(updatedSeed.getId())
+			.seedName(updatedSeed.getSeedName())
+			.build();
 	}
 
 	private SeedProjectionResult getSeedProjectionResult(List<Long> seedIds) {
@@ -640,6 +705,11 @@ public class SeedService {
 		return tagProjections.stream()
 				.collect(Collectors.groupingBy(SeedTagProjection::getSeedId,
 						Collectors.mapping(SeedTagProjection::getTagName, Collectors.toList())));
+	}
+
+	private Seed getSeedOrThrow(Long seedId) {
+		return seedRepository.findById(seedId)
+			.orElseThrow(() -> SeedzipException.from(ErrorCode.SEED_NOT_FOUND));
 	}
 
 }
