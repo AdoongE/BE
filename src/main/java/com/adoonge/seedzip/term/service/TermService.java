a377b4dbfef5c432b12120317451fa165a0fc95f
gyuseon <mn5592@naver.com>
2025-05-10 18:21:46 +0900

    Feat: 약관 수정 기능 구현 (#134)

--- src/main/java/com/adoonge/seedzip/term/service/TermService.java
+++ src/main/java/com/adoonge/seedzip/term/service/TermService.java
@@ -2,12 +2,16 @@ package com.adoonge.seedzip.term.service;
 
 import com.adoonge.seedzip.global.exception.ErrorCode;
 import com.adoonge.seedzip.global.exception.SeedzipException;
+import com.adoonge.seedzip.member.domain.Member;
+import com.adoonge.seedzip.member.domain.Role;
+import com.adoonge.seedzip.term.dto.request.TermUpdateRequest;
 import com.adoonge.seedzip.term.dto.response.TermResponse;
 import com.adoonge.seedzip.term.entity.Term;
 import com.adoonge.seedzip.term.entity.TermType;
 import com.adoonge.seedzip.term.repository.TermRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,9 +19,22 @@ public class TermService {
 
     private final TermRepository termRepository;
 
+    @Transactional(readOnly = true)
     public TermResponse getTermByType(TermType type) {
         Term term = termRepository.findByType(type)
                 .orElseThrow(() -> SeedzipException.from(ErrorCode.TERM_NOT_FOUND));
         return TermResponse.from(term);
+    }
+
+    @Transactional
+    public void updateTerm(TermType type, TermUpdateRequest request, Member member) {
+
+        if(!member.getRole().equals(Role.USER)) {
+            throw SeedzipException.from(ErrorCode.MEMBER_NOT_ADMIN);
+        }
+
+        Term term = termRepository.findByType(type)
+                .orElseThrow(() -> SeedzipException.from(ErrorCode.TERM_NOT_FOUND));
+        term.update(request.title(), request.content());
     }
 }
