c5b1d0afbbfeba9e03611c88d53cd3da468dd23f
chaen-ing <kimce0405@naver.com>
2025-05-17 15:31:41 +0900

    refactor : 카테고리 서비스 변경 (#137)

--- src/main/java/com/adoonge/seedzip/seed/repository/SeedRepository.java
+++ src/main/java/com/adoonge/seedzip/seed/repository/SeedRepository.java
@@ -3,19 +3,31 @@ package com.adoonge.seedzip.seed.repository;
 import com.adoonge.seedzip.member.domain.Member;
 import com.adoonge.seedzip.seed.domain.Seed;
 import com.adoonge.seedzip.seed.domain.SeedType;
+
 import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-public interface SeedRepository extends JpaRepository<Seed, Long> {
-    Page<Seed> findByMember(Member member, Pageable pageable);
-    Page<Seed> findByMemberAndSeedType(Member member, SeedType seedType, Pageable pageable);
+import jakarta.transaction.Transactional;
 
-    @Query("SELECT s FROM Seed s WHERE s.id IN :seedIds")
-    Page<Seed> findBySeedIdIn(List<Long> seedIds, Pageable pageable);
-    @Query("SELECT s FROM Seed s WHERE s.id IN :seedIds AND s.seedType = :seedType")
-    Page<Seed> findBySeedIdInAndSeedType(List<Long> seedIds, SeedType seedType, Pageable pageable);
+public interface SeedRepository extends JpaRepository<Seed, Long> {
+	Page<Seed> findByMember(Member member, Pageable pageable);
+
+	Page<Seed> findByMemberAndSeedType(Member member, SeedType seedType, Pageable pageable);
+
+	@Query("SELECT s FROM Seed s WHERE s.id IN :seedIds")
+	Page<Seed> findBySeedIdIn(List<Long> seedIds, Pageable pageable);
+
+	@Query("SELECT s FROM Seed s WHERE s.id IN :seedIds AND s.seedType = :seedType")
+	Page<Seed> findBySeedIdInAndSeedType(List<Long> seedIds, SeedType seedType, Pageable pageable);
+
+	@Transactional
+	@Modifying
+	@Query("DELETE FROM Seed s WHERE NOT EXISTS (SELECT 1 FROM CategorySeed cs WHERE cs.seed.id = s.id)")
+	default void deleteUnreferencedContents() {}
 
 }
